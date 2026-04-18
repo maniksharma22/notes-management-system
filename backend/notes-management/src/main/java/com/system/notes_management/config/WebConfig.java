7c8780616af66ce9b457bed70a1c6c3bd5f1db48
Manik Sharma <114878881+maniksharma22@users.noreply.github.com>
2026-04-19 00:56:13 +0530

    Update WebConfig.java

--- backend/notes-management/src/main/java/com/system/notes_management/config/WebConfig.java
+++ backend/notes-management/src/main/java/com/system/notes_management/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://noteflow-app-seven.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

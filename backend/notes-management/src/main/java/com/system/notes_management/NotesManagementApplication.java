a90dea5011ff92678d4ec364cec4ed8fde85a42c
Manik Sharma <114878881+maniksharma22@users.noreply.github.com>
2026-04-19 03:37:25 +0530

    Update NotesManagementApplication.java

--- backend/notes-management/src/main/java/com/system/notes_management/NotesManagementApplication.java
+++ backend/notes-management/src/main/java/com/system/notes_management/NotesManagementApplication.java
@@ -1,10 +1,17 @@
 package com.system.notes_management;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotesManagementApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesManagementApplication.class, args);
